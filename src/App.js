bd9d56dea206f9c3d8f935bd8fc309c3b969a6d3
Gyeongwon-Gim <gyeongwon7986@gmail.com>
2024-09-01 07:42:12 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import profile from "./img_profile.png";
 function App() {
+  const greetings = ["안녕하세요!", "Hello!", "Ciao!"];
+  const [currentGreeting, setCurrentGreeting] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentGreeting((prevIndex) => (prevIndex + 1) % greetings.length);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img id="profile" src={profile} alt="프로필 이미지"></img>
+        {/* <button id="themeToggle">switch</button> */}
       </header>
+      <h1 id="blinking-text">{greetings[currentGreeting]}</h1>
     </div>
   );
 }
